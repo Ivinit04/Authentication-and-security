d5579fef7a72b63e866d35acb384516201333048
vinit <vinitjoshiofficial@gmail.com>
2023-10-26 21:10:59 +0530

    Level 5 - session-based authentication using passport.js to add cookies and sessions  which only remembers the login status within a single session (until the user closes the browser). If you want to remember the login status across browser sessions (even after closing and reopening the browser), you would indeed need to implement a persistent and secure login mechanism.

--- app.js
+++ app.js
@@ -4,23 +4,44 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
-const bcrypt = require('bcrypt');
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 const port = 3000;
-
-mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
 app.use(express.static("public"));
 app.set("view engine" , "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+//Initialize session
+app.use(session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false
+}));
+
+//Initialize passport
+app.use(passport.initialize());
+//Managing login sessions using passport
+app.use(passport.session());
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
-})
+});
+
+userSchema.plugin(passportLocalMongoose);  //salt and hash password and save users into mongoDB database
 
 const User = new mongoose.model("User" , userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/" , (req ,res)=>{
     res.render("home");
@@ -34,35 +55,38 @@ app.get("/login" , (req , res)=>{
     res.render("login");
 })
 
-app.post("/register" , async (req , res)=>{
-    // console.log(req.body);
-    const userPassword = req.body.password;
-    const hashPassword = await bcrypt.hash(userPassword , 10);   //hashed password
-    const user = new User({
-        email: req.body.username,
-        password: hashPassword  
-    })
-    user.save();
-    res.render("secrets");
+app.get("/secrets" , (req , res)=>{
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    } else{
+        res.redirect("/login");
+    }
 })
 
-app.post("/login" , async (req , res)=>{
+app.get("/logout" , (req , res)=>{
+    req.logout((err) => {
+        if (err) {
+            // Handle any logout errors, e.g., by showing an error message or redirecting to an error page.
+            console.error(err);
+        }
+        // Redirect to the desired page (e.g., the home page) after successful logout.
+        res.redirect("/");
+    });
+})
+
+app.post("/register" , async (req , res)=>{
     // console.log(req.body);
-    const userEmail = req.body.username;
-    const userPassword = req.body.password;
-    const userExists = await User.findOne({email: userEmail});
-    
-    if(userExists) {
-        if(await bcrypt.compare(userPassword , userExists.password)){   //comparing hashed password with the password user entered
-            res.render("secrets");
-        }
-        else{
-            res.redirect("login");
-        }
-    }else{
-        console.log("user not exists");
-    }
-});
+    const user = await User.register({username: req.body.username} , req.body.password);
+    // If registration is successful, authenticate the user
+    passport.authenticate("local")(req ,res , ()=>{
+        res.redirect("/secrets");
+    })
+})
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secrets",  // Redirect to this route on successful login
+    failureRedirect: "/login",   // Redirect to this route on failed login
+}));
 
 app.listen(port , ()=>{
     console.log(`Server started on port ${port}`);
